d078d88b19ea3a270e83dc460e6bfd6fe8d43d96
pascoalwiseu-prog <pascoalwiseu@gmail.com>
2026-07-09 15:45:51 +0100

    Add enviar function to submit data to Firestore

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function enviar(){
 
+const marca=document.getElementById("marca").value;
+const numero=document.getElementById("numero").value;
+
+db.collection("respostas").add({
+marca:marca,
+numero:numero,
+data:new Date()
+});
+
+document.getElementById("msg").innerHTML="Enviado!";
+}
