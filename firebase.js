ef9edfb71b64c6719bf0663f1e1ccf1cf4227ba0
pascoalwiseu-prog <pascoalwiseu@gmail.com>
2026-07-18 09:24:16 +0100

    Change Firestore to Realtime Database in firebase.js
    Updated Firebase configuration to use Realtime Database instead of Firestore.

--- firebase.js
+++ firebase.js
@@ -1,9 +1,14 @@
 const firebaseConfig = {
-
-// COLE AQUI A CONFIGURAÇÃO DO FIREBASE
-
+  apiKey: "AIzaSyAJ_vqVBZ7bJtXCPhKtGcBDzKefSsfo7hc",
+  authDomain: "magic-bot-7da83.firebaseapp.com",
+  databaseURL: "https://magic-bot-7da83-default-rtdb.firebaseio.com",
+  projectId: "magic-bot-7da83",
+  storageBucket: "magic-bot-7da83.firebasestorage.app",
+  messagingSenderId: "426536016846",
+  appId: "1:426536016846:web:d412ffbcd43021b98ea99f",
+  measurementId: "G-GZ5VX31LBK"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const db = firebase.firestore();
+const db = firebase.database();
