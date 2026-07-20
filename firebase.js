83ed14246706718a395c5869f6e98b8377f3c4c8
pascoalwiseu-prog <pascoalwiseu@gmail.com>
2026-07-20 08:28:01 +0100

    Update Firebase API key and add auth initialization

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAJ_vqVBZ7bJtXCPhKtGcBDzKefSsfo7hc",
+  apiKey: "SUA_API_KEY",
   authDomain: "magic-bot-7da83.firebaseapp.com",
   databaseURL: "https://magic-bot-7da83-default-rtdb.firebaseio.com",
   projectId: "magic-bot-7da83",
   storageBucket: "magic-bot-7da83.firebasestorage.app",
   messagingSenderId: "426536016846",
-  appId: "1:426536016846:web:d412ffbcd43021b98ea99f",
-  measurementId: "G-GZ5VX31LBK"
+  appId: "1:426536016846:web:d412ffbcd43021b98ea99f"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database();
+const auth = firebase.auth();
