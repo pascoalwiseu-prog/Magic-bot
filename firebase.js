f2c7ba53c04634475e31e03184bf58a3092922eb
pascoalwiseu-prog <pascoalwiseu@gmail.com>
2026-07-20 08:29:53 +0100

    Update Firebase config with new API key and measurement ID
    Updated Firebase configuration with new API key and added measurement ID.

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "AIzaSyAJ_vqVBZ7bJtXCPhKtGcBDzKefSsfo7hc",
   authDomain: "magic-bot-7da83.firebaseapp.com",
   databaseURL: "https://magic-bot-7da83-default-rtdb.firebaseio.com",
   projectId: "magic-bot-7da83",
   storageBucket: "magic-bot-7da83.firebasestorage.app",
   messagingSenderId: "426536016846",
-  appId: "1:426536016846:web:d412ffbcd43021b98ea99f"
+  appId: "1:426536016846:web:d412ffbcd43021b98ea99f",
+  measurementId: "G-GZ5VX31LBK"
 };
 
 firebase.initializeApp(firebaseConfig);
